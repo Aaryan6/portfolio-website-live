06a448f8b1c60024c5561a7fd8f3a4ce516ca7b4
Aaryan6 <aaryanpatel683@gmail.com>
2023-11-02 17:46:15 +0530

    sanity conf

---
+++ dist/static/index-27f558d4-28bc0a72.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-f6f16e84-ce29645b.js";import{D as m}from"./sanity-1d17ee51.js";import"./json-inspector-2c379b42.js";export{m as DocumentPaneProvider,t as default};
